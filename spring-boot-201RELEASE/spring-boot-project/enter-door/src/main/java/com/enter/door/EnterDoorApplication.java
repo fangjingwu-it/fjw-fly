b6987386867c10f7bcef2603794c584c30e48686
fangjingwu <fangjingwu05865@hellobike.com>
2020-01-14 16:56:51 +0800

    SpringBoot2.0.1.RELESAE

---
+++ spring-boot-201RELEASE/spring-boot-project/enter-door/src/main/java/com/enter/door/EnterDoorApplication.java
@@ -0,0 +1,15 @@
+package com.enter.door;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @description: 入口
+ * @author: jingw F
+ * @date: 2020-01-14 15:24
+ **/
+public class EnterDoorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterDoorApplication.class, args);
+	}
+}
